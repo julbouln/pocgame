f5ca3596bcc013a6817e2fcf0e517cf607f126f0
julbouln <jboulnois@immateriel.fr>
2005-01-27 14:31:00 +0000

    fix

--- game_decor.ml
+++ game_decor.ml
@@ -9,12 +9,12 @@ object (self)
   inherit game_object nm w h file false false cw ch as super
 
   initializer 
-    super#set_blocking true;
-   if stc#is_state "idle" then (
-     let idle_state=new state_object "idle" (stc#get_state "idle").frames (stc#get_state "idle").refresh in
-       idle_state#set_action (fun()->());
-       state_manager#add_state idle_state;
-   );
+(*    super#set_blocking true; *)
+    if stc#is_state "idle" then (
+      let idle_state=new state_object "idle" (stc#get_state "idle").frames (stc#get_state "idle").refresh in
+	idle_state#set_action (fun()->());
+	state_manager#add_state idle_state;
+    );
    self#set_state "idle";
   
 end;;
